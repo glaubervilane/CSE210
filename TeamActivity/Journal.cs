8678d2761da2a3348673704173699c37746be427
yourusernameglaubervilanegit config --global user.email glauberfmarques@gmail.comgit config --global user.name yourusernameglaubervilane <glauberfmarques@gmail.com>
2023-10-04 19:53:11 -0600

    Update Journal class to catch exceptions or errors

--- TeamActivity/Journal.cs
+++ TeamActivity/Journal.cs
@@ -55,29 +55,42 @@ class Journal
   public void LoadFromFile(string fileName)
   {
     entries.Clear();
-    using (StreamReader reader = new StreamReader(fileName))
-    {
-      string line;
-      string currentPrompt = null;
-      string currentResponse = null;
-      DateTime currentDate = DateTime.MinValue;
 
-      while ((line = reader.ReadLine()) != null)
+    try
+    {
+      using (StreamReader reader = new StreamReader(fileName))
       {
-        if (line.StartsWith("Date: "))
+        string line;
+        string currentPrompt = null;
+        string currentResponse = null;
+        DateTime currentDate = DateTime.MinValue;
+
+        while ((line = reader.ReadLine()) != null)
         {
-          currentDate = DateTime.Parse(line.Substring(6));
-        }
-        else if (line.StartsWith("Prompt: "))
-        {
-          currentPrompt = line.Substring(8);
-        }
-        else if (line.StartsWith("Response: "))
-        {
-          currentResponse = line.Substring(10);
-          entries.Add(new Entry(currentPrompt, currentResponse) { Date = currentDate });
+          if (line.StartsWith("Date: "))
+          {
+            currentDate = DateTime.Parse(line.Substring("Date: ".Length));
+          }
+          else if (line.StartsWith("Prompt: "))
+          {
+            currentPrompt = line.Substring("Prompt: ".Length);
+          }
+          else if (line.StartsWith("Response: "))
+          {
+            currentResponse = line.Substring("Response: ".Length);
+            entries.Add(new Entry(currentPrompt, currentResponse)
+            {
+              Date = currentDate,
+              Prompt = currentPrompt,
+              Response = currentResponse
+            });
+          }
         }
       }
+    }
+    catch (Exception ex)
+    {
+      Console.WriteLine($"An error occurred while loading the journal: {ex.Message}");
     }
   }
 
