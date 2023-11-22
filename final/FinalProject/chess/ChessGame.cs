779a0d27cebd0acaeebea008b042be853915893f
yourusernameglaubervilanegit config --global user.email glauberfmarques@gmail.comgit config --global user.name yourusernameglaubervilane <glauberfmarques@gmail.com>
2023-11-21 17:17:34 -0700

    Begin inplementing get pieces on board game

--- final/FinalProject/chess/ChessGame.cs
+++ final/FinalProject/chess/ChessGame.cs
@@ -1,4 +1,5 @@
-using System.ComponentModel.DataAnnotations.Schema;
+using System.Collections.Generic;
+using System.IO.Pipes;
 using FinalProject.board;
 
 namespace FinalProject.chess
@@ -9,6 +10,8 @@ namespace FinalProject.chess
         public int turn { get; private set; }
         public Color actualPlayer { get; private set; }
         public bool gameFinish { get; private set; }
+        public HashSet<Piece> pieces { get; set; }
+        public HashSet<Piece> captured { get; set; }
 
         public ChessGame()
         {
@@ -16,6 +19,8 @@ namespace FinalProject.chess
             turn = 1;
             actualPlayer = Color.White;
             gameFinish = false;
+            pieces = new HashSet<Piece>();
+            captured = new HashSet<Piece>();
             putPieces();
         }
 
@@ -25,18 +30,22 @@ namespace FinalProject.chess
             p.incrementMovements();
             Piece capturedPiece = board.takeOffPiece(destination);
             board.putPiece(p, destination);
+            if (capturedPiece != null)
+            {
+                captured.Add(capturedPiece);
+            }
         }
 
-        public void makePlay(Position origin, Position destination) 
+        public void makePlay(Position origin, Position destination)
         {
             move(origin, destination);
             turn++;
             changePlayer();
         }
 
-        public void validateOriginPosition(Position pos) 
-        { 
-            if (board.piece(pos) == null )
+        public void validateOriginPosition(Position pos)
+        {
+            if (board.piece(pos) == null)
             {
                 throw new BoardException("There is no piece in the chosen origin position!");
             }
@@ -50,8 +59,8 @@ namespace FinalProject.chess
             }
         }
 
-        public void validateDestinationPosition(Position origin, Position destination) 
-        { 
+        public void validateDestinationPosition(Position origin, Position destination)
+        {
             if (!board.piece(origin).canMoveTo(destination))
             {
                 throw new BoardException("Destination invalid!");
@@ -64,26 +73,60 @@ namespace FinalProject.chess
             {
                 actualPlayer = Color.Black;
             }
-            else 
+            else
             {
                 actualPlayer = Color.White;
             }
         }
-        private void putPieces() 
-        {
-            board.putPiece(new Rook(board, Color.White), new ChessPosition('c',1).toPosition());
-            board.putPiece(new Rook(board, Color.White), new ChessPosition('c',2).toPosition());
-            board.putPiece(new Rook(board, Color.White), new ChessPosition('d',2).toPosition());
-            board.putPiece(new Rook(board, Color.White), new ChessPosition('e',1).toPosition());
-            board.putPiece(new Rook(board, Color.White), new ChessPosition('e',2).toPosition());
-            board.putPiece(new King(board, Color.White), new ChessPosition('d',1).toPosition());
 
-            board.putPiece(new Rook(board, Color.Black), new ChessPosition('c',7).toPosition());
-            board.putPiece(new Rook(board, Color.Black), new ChessPosition('c',8).toPosition());
-            board.putPiece(new Rook(board, Color.Black), new ChessPosition('d',7).toPosition());
-            board.putPiece(new Rook(board, Color.Black), new ChessPosition('e',7).toPosition());
-            board.putPiece(new Rook(board, Color.Black), new ChessPosition('e',8).toPosition());
-            board.putPiece(new King(board, Color.Black), new ChessPosition('d',8).toPosition());
+        public HashSet<Piece> capturedPieces(Color color)
+        {
+            HashSet<Piece> aux = new HashSet<Piece>();
+            foreach (Piece x in captured)
+            {
+                if (x.color == color)
+                {
+                    aux.Add(x);
+                }
+            }
+            return aux;
+        }
+
+        public HashSet<Piece> onGamePieces(Color color)
+        {
+            HashSet<Piece> aux = new HashSet<Piece>();
+            foreach (Piece x in pieces)
+            {
+                if (x.color == color)
+                {
+                    aux.Add(x);
+                }
+            }
+            aux.Except(capturedPieces(color));
+            return aux;
+        }
+
+        public void putNewPiece(char column, int row, Piece piece)
+        {
+            board.putPiece(piece, new ChessPosition(column, row).toPosition());
+            pieces.Add(piece);
+        }
+        private void putPieces()
+        {
+            putNewPiece('c', 1, new Rook(board, Color.White));
+            putNewPiece('c', 2, new Rook(board, Color.White));
+            putNewPiece('d', 2, new Rook(board, Color.White));
+            putNewPiece('e', 1, new Rook(board, Color.White));
+            putNewPiece('e', 2, new Rook(board, Color.White));
+            putNewPiece('d', 1, new King(board, Color.White));
+
+            putNewPiece('c', 7, new Rook(board, Color.Black));
+            putNewPiece('c', 8, new Rook(board, Color.Black));
+            putNewPiece('d', 7, new Rook(board, Color.Black));
+            putNewPiece('e', 7, new Rook(board, Color.Black));
+            putNewPiece('e', 8, new Rook(board, Color.Black));
+            putNewPiece('d', 8, new King(board, Color.Black));
+
         }
     }
 }
